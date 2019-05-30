e9a980b26a6e2f0092208677aff996263d89390e
DeathChaos25 <deathchaos25@gmail.com>
2019-05-30 16:14:53 -0400

    Add files via upload

---
+++ source/PQ1ModCPK_asm.s
@@ -0,0 +1,23 @@
+.text
+.align 4
+
+AddNewModCPK:
+    mov r0, #1
+	bl 0x103F34
+    adr r0, ModCPK
+    bl 0x104684
+    mov r0, #2
+	bl 0x103F34
+    adr r0, DataCPK
+    bl 0x104684
+    b 0x101F1C
+
+.align 2
+
+DataCPK:
+.asciz "data.cpk"
+ModCPK:
+.asciz "mod.cpk"
+
+CreditsString:
+.asciz "This name patch was made by DeathChaos"
