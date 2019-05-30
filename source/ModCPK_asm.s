ac109c5cbb3ad01f9b2fabc5d5228c58afdfe3c2
DeathChaos25 <deathchaos25@gmail.com>
2019-05-30 17:24:25 -0400

    magikoopa hates adr instructions
    moved adr instructions to ldr to stop the patch from randomly complaining and not building

---
+++ source/ModCPK_asm.s
@@ -0,0 +1,22 @@
+.text
+.align 4
+
+AddNewModCPK:
+	mov r0, #1
+	bl 0x103F34
+	ldr r0, =ModCPK
+	bl 0x104684
+	mov r0, #2
+	bl 0x103F34
+	ldr r0, =DataCPK
+	bl 0x104684
+	b 0x101F1C
+
+.align 2
+
+DataCPK:
+.asciz "data.cpk"
+ModCPK:
+.asciz "mod.cpk"
+CreditsString:
+.asciz "This patch was made by DeathChaos"
